4e155ef38629c2a9c6251c579e23fcb01cb11f46
darmond157 <bardiarezaeikalantari@gmail.com>
2024-07-03 11:24:37 +0330

    chore: remove unnecessary codes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ fastify.register(require("@fastify/postgres"), {
 	connectionString: "postgres://postgres@localhost/postgres",
 });
 
-fastify.post("/voice-detail", async (req, res) => {
+fastify.post("/voice-details", async (req, res) => {
 	const { name, description, date } = req.body;
 	if (!(name && description && date))
 		return res.send("please provide all fields ...");
@@ -21,6 +21,39 @@ fastify.post("/voice-detail", async (req, res) => {
 		});
 });
 
-fastify.get("/voice-details");
+fastify.get("/voice-details/:name", async (req, res) => {
+	const name = req.params.name;
+	await fastify.pg
+		.query("SELECT * FROM voiceDetails WHERE name=$1", [name])
+		.then((result) => {
+			res.send(result.rows[0]);
+		})
+		.catch((err) => {
+			res.send(err);
+		});
+});
+
+fastify.delete("/voice-details/:name", async (req, res) => {
+	const name = req.params.name;
+	await fastify.pg
+		.query("DELETE FROM voiceDetails WHERE name=$1", [name])
+		.then(() => {
+			res.send("voice detail deleted successfuly ...");
+		})
+		.catch((err) => {
+			res.send(err);
+		});
+});
+
+fastify.get("/voice-details", async (req, res) => {
+	await fastify.pg
+		.query("SELECT * FROM voiceDetails")
+		.then((result) => {
+			res.send(result.rows);
+		})
+		.catch((err) => {
+			res.send(err);
+		});
+});
 
 fastify.listen({ port: 3000, host: "0.0.0.0" });
